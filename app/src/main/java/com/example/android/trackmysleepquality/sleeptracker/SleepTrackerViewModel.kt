a18e7bd8aada2e1370098f21f55626a02d4950d4
Urmaturi <urmasuper93@gmail.com>
2022-03-03 02:23:30 +0600

    add navigation

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -18,10 +18,7 @@ package com.example.android.trackmysleepquality.sleeptracker
 
 import android.app.Application
 import android.provider.SyncStateContract.Helpers.update
-import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
+import androidx.lifecycle.*
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 import com.example.android.trackmysleepquality.database.SleepNight
@@ -48,6 +45,12 @@ class SleepTrackerViewModel(
     private val nights = database.getAllNight()
     val nightsString = Transformations.map(nights) { nights ->
         formatNights(nights, application.resources)
+    }
+    private val _navigateToSleepQuality = MutableLiveData<SleepNight>()
+    val navigateTosleepQuality: LiveData<SleepNight>
+    get()= _navigateToSleepQuality
+    fun doneNavigating(){
+        _navigateToSleepQuality.value = null
     }
 
     init {
@@ -108,7 +111,9 @@ class SleepTrackerViewModel(
             val oldNight = tonight.value ?: return@launch
             oldNight.endTimeMilli = System.currentTimeMillis()
             update(oldNight)
+            _navigateToSleepQuality.value = oldNight
         }
+
     }
 
     private suspend fun update(night: SleepNight) {
